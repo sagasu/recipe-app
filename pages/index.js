5e3be056ddeb9a4aa884b971d9ffef1e8accb231
Mateusz Kopij <sagasu@gmail.com>
2021-06-03 08:41:57 +0100

    adding link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
-export default function Home() {
+export default function Home({data}) {
+const recipes = data.recipes;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,8 +16,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Recipes
         </h1>
+        <Link href="/about"><a>{recipes[0].title}</a>
+        </Link> 
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -68,12 +73,12 @@ export default function Home() {
   )
 }
 
-export function getStaticProps {
+export function getStaticProps() {
   return {
     props: {
       data: {
-        recipes: []
-      }
-    }
-  }
+        recipes: [{title: "Pineapple"}]
+      },
+    },
+  };
 }
